d061215d3fc7a90fc39a961d0d8b2105358ca495
parthiban <parthiban@vthink.co.in>
2023-07-15 11:36:18 +0530

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,184 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# [
+#   { name: 'Home' }, { name: 'Work' }, { name: 'Other' }
+# ].each do |address_type|
+#   unless AddressType.where(name: address_type[:name]).present?
+#     AddressType.create!(address_type)
+#   end
+# end
+
+[
+    {username: "Prakash krishnasamy", rollnumber: "VT105", role: "developer"},
+    {username: "Satheesh kumar", rollnumber: "VT169", role: "developer"},
+    {username: "Kanagha dhara", rollnumber: "VT205", role: "developer"},
+    {username: "Ramani", rollnumber: "VT206", role: "developer"},
+    {username: "Senthamizhselvi", rollnumber: "VT207", role: "developer"},
+    {username: "Saravanan kathiravan", rollnumber: "VT209", role: "developer"},
+    {username: "Hariharan Sivashanmugam", rollnumber: "VT129", role: "developer"},
+    {username: "Siva suriya", rollnumber: "VT164", role: "developer"},
+    {username: "Ragha malliga", rollnumber: "VT216", role: "developer"},
+    {username: "Sukumar", rollnumber: "T0005", role: "developer"},
+    {username: "Kumaravelthondaman", rollnumber: "VT119", role: "developer"},
+    {username: "Ramkumar somasundarapandian", rollnumber: "VT124", role: "developer"},
+    {username: "Praveen kumar sridhar", rollnumber: "VT135", role: "developer"},
+    {username: "Rathinakumar saravanan", rollnumber: "VT148", role: "developer"},
+    {username: "Praveen lingaraj", rollnumber: "VT156", role: "developer"},
+    {username: "Dhinesh kumar", rollnumber: "VT158", role: "developer"},
+    {username: "Logeswaran", rollnumber: "VT160", role: "developer"},
+    {username: "Kihor", rollnumber: "VT162", role: "developer"},
+    {username: "Hemalatha rajendiran", rollnumber: "VT053", role: "developer"},
+    {username: "Thameem ansari", rollnumber: "VT195", role: "developer"},
+    {username: "Sivakami", rollnumber: "VT212", role: "developer"},
+    {username: "Vanmathi", rollnumber: "VT214", role: "developer"},
+    {username: "Saraniya shanmugasundaram", rollnumber: "VT184", role: "developer"},
+    {username: "Kiruthika", rollnumber: "VT189", role: "developer"},
+    {username: "Jeyapriya", rollnumber: "VT180", role: "developer"},
+    {username: "Vengadesh", rollnumber: "VT197", role: "developer"},
+    {username: "Rajesh babu", rollnumber: "VT012", role: "lead"},
+    {username: "Vignesh", rollnumber: "VT016", role: "lead"},
+    {username: "Balaji", rollnumber: "VT018", role: "lead"},
+    {username: "Sasikala balasubramanian", rollnumber: "VT074", role: "lead"},
+    {username: "Devendran paramasivam", rollnumber: "VT122", role: "lead"},
+    {username: "Baskar", rollnumber: "VT136", role: "lead"},
+    {username: "Mohammad irfan nazeer", rollnumber: "VT150", role: "lead"},
+    {username: "Senthil kumar", rollnumber: "VT199", role: "lead"},
+    {username: "Shiny Velupillai",rollnumber: "VT208", role: "developer"},
+    {username: "Priyadharshini", rollnumber: "VT215", role: ""},
+    {username: "Ombalakumar", rollnumber: "VT027", role: "developer"},
+    {username: "Amaran Anbazhakan", rollnumber: "VT101", role: "developer"},
+    {username: "Venkataramani Marichelvam", rollnumber: "VT146", role: "developer"},
+    {username: "Sneka", rollnumber: "VT181", role: "developer"},
+    {username: "Deenadayalan Thiruvengadam", rollnumber: "VT192", role: "developer"},
+    {username: "Priyadharshini", rollnumber: "VT210", role: "developer"},
+    {username: "Dhandhayothapani Arumugam", rollnumber: "VT006", role: "developer"},
+    {username: "Sridhar Rajendran", rollnumber: "VT009", role: "developer"},
+    {username: "Sakthivel", rollnumber: "VT078", role: "developer"},
+    {username: "Parthiban Ramasamy", rollnumber: "VT081", role: "developer"},
+    {username: "Varsha Ravi", rollnumber: "VT111", role: "developer"},
+    {username: "Saravanakumar Umapthi", rollnumber: "VT159", role: "developer"},
+    {username: "Revathy Jeyakumar", rollnumber: "VT165", role: "developer"},
+    {username: "Surenthran", rollnumber: "VT173", role: "developer"},
+    {username: "Muthunarayani", rollnumber: "VT178", role: "developer"},
+    {username: "Priyanka", rollnumber: "VT211", role: "developer"},
+    {username: "Govardhananan", rollnumber: "VT220", role: "developer"},
+    {username: "Soumiksen", rollnumber: "VT221", role: "developer"},
+    {username: "Harishkumar", rollnumber: "VT222", role: "developer"},
+    {username: "Jegan Arul", rollnumber: "VT102", role: "developer"},
+    {username: "Dinesh Nehru", rollnumber: "VT151", role: "developer"},
+    {username: "Balaganeshwaran", rollnumber: "VT155", role: "developer"},
+    {username: "Vignesh Selvabharathi", rollnumber: "VT170", role: "developer"},
+    {username: "Ajithkumar paramashivam", rollnumber: "VT062", role: "developer"},
+    {username: "Sanjeevkumar", rollnumber: "VT118", role: "developer"},
+    {username: "Prabhu Selvaraj", rollnumber: "VT125", role: "developer"},
+    {username: "Sebastian nisholin", rollnumber: "VT130", role: "developer"},
+    {username: "Logeshwari sarangan", rollnumber: "VT132", role: "developer"},
+    {username: "Pichaimuthu kanagaraj", rollnumber: "VT134", role: "developer"},
+    {username: "Dineshkumar Aanathachari", rollnumber: "VT152", role: "developer"},
+    {username: "Naveen Raja", rollnumber: "VT200", role: "developer"},
+    {username: "Vindhiya", rollnumber: "VT217", role: "developer"}
+  
+
+    
+
+].each do | user |
+    unless User.where(rollnumber: user[:rollnumber]).present?
+        User.create!(user)
+    end
+
+end
+
+[
+    {username: "Prakash krishnasamy", rollnumber: "VT105", role: "developer",projectname:"AHFO"},
+    {username: "Satheesh kumar", rollnumber: "VT169", role: "developer",projectname:"AHFO"},
+    {username: "Kanagha dhara", rollnumber: "VT205", role: "developer",projectname:"AHFO"},
+    {username: "Ramani", rollnumber: "VT206", role: "developer",projectname:"AHFO"},
+    {username: "Senthamizhselvi", rollnumber: "VT207", role: "developer",projectname:"AHFO"},
+    {username: "Saravanan kathiravan", rollnumber: "VT209", role: "developer",projectname:"AHFO"},
+    {username: "Hariharan Sivashanmugam", rollnumber: "VT129", role: "developer",projectname:"DESIGN"},
+    {username: "Siva suriya", rollnumber: "VT164", role: "developer",projectname:"DESIGN"},
+    {username: "Ragha malliga", rollnumber: "VT216", role: "developer",projectname:"DESIGN"},
+    {username: "Sukumar", rollnumber: "T0005", role: "developer",projectname:"SYSTEMS"},
+    {username: "Kumaravelthondaman", rollnumber: "VT119", role: "developer",projectname:"ACRT/BM"},
+    {username: "Ramkumar somasundarapandian", rollnumber: "VT124", role: "developer",projectname:"ACRT/BM"},
+    {username: "Praveen kumar sridhar", rollnumber: "VT135", role: "developer",projectname:"BM"},
+    {username: "Rathinakumar saravanan", rollnumber: "VT148", role: "developer",projectname:"ACRT/BM"},
+    {username: "Praveen lingaraj", rollnumber: "VT156", role: "developer",projectname:"ACRT/BM"},
+    {username: "Dhinesh kumar", rollnumber: "VT158", role: "developer",projectname:"VRAP"},
+    {username: "Logeswaran", rollnumber: "VT160", role: "developer",projectname:"ACRT/BM"},
+    {username: "Kihor", rollnumber: "VT162", role: "developer",projectname:"ACRT/BM"},
+    {username: "Hemalatha rajendiran", rollnumber: "VT053", role: "developer",projectname:"PATHFACTORY"},
+    {username: "Thameem ansari", rollnumber: "VT195", role: "developer",projectname:"SYSTEMS"},
+    {username: "Sivakami", rollnumber: "VT212", role: "developer",projectname:"ACRT"},
+    {username: "Vanmathi", rollnumber: "VT214", role: "developer",projectname:"ACRT"},
+    {username: "Saraniya shanmugasundaram", rollnumber: "VT184", role: "developer",projectname:"ALERTE-IAM"},
+    {username: "Kiruthika", rollnumber: "VT189", role: "developer",projectname:"ALERTE-IAM"},
+    {username: "Jeyapriya", rollnumber: "VT180", role: "developer",projectname:"ALERTE-DEVOPS"},
+    {username: "Vengadesh", rollnumber: "VT197", role: "developer",projectname:"ALERTE-DEVOPS"},
+    {username: "Rajesh babu", rollnumber: "VT012", role: "lead",projectname:"ACRT/BM"},
+    {username: "Vignesh", rollnumber: "VT016", role: "lead",projectname:"DEOLDATA"},
+    {username: "Balaji", rollnumber: "VT018", role: "lead",projectname:"DESIGN"},
+    {username: "Sasikala balasubramanian", rollnumber: "VT074", role: "lead",projectname:"ECSITE"},
+    {username: "Devendran paramasivam", rollnumber: "VT122", role: "lead",projectname:"ALERTE-IAM"},
+    {username: "Baskar", rollnumber: "VT136", role: "lead",projectname:"ACRT/BM"},
+    {username: "Mohammad irfan nazeer", rollnumber: "VT150", role: "lead",projectname:"ALERTE-DEVOPS"},
+    {username: "Senthil kumar", rollnumber: "VT199", role: "lead",projectname:"ACRT/BM"},
+    {username: "Shiny Velupillai",rollnumber: "VT208", role: "developer",projectname:"BM"},
+    {username: "Priyadharshini", rollnumber: "VT215", role: "",projectname:""},
+    {username: "Ombalakumar", rollnumber: "VT027", role: "developer",projectname:"ACRT/BM"},
+    {username: "Amaran Anbazhakan", rollnumber: "VT101", role: "developer",projectname:"ACRT/BM"},
+    {username: "Venkataramani Marichelvam", rollnumber: "VT146", role: "developer",projectname:"ACRT/BM"},
+    {username: "Sneka", rollnumber: "VT181", role: "developer",projectname:"ACRT/BM"},
+    {username: "Deenadayalan Thiruvengadam", rollnumber: "VT192", role: "developer",projectname:"BM"},
+    {username: "Priyadharshini", rollnumber: "VT210", role: "developer",projectname:"ACRT"},
+    {username: "Dhandhayothapani Arumugam", rollnumber: "VT006", role: "developer",projectname:"ECSITE"},
+    {username: "Sridhar Rajendran", rollnumber: "VT009", role: "developer",projectname:"ECSITE"},
+    {username: "Sakthivel", rollnumber: "VT078", role: "developer",projectname:"ECSITE"},
+    {username: "Parthiban Ramasamy", rollnumber: "VT081", role: "developer",projectname:"ECSITE"},
+    {username: "Varsha Ravi", rollnumber: "VT111", role: "developer",projectname:"BM-PROD"},
+    {username: "Saravanakumar Umapthi", rollnumber: "VT159", role: "developer",projectname:"ACRT/BM"},
+    {username: "Revathy Jeyakumar", rollnumber: "VT165", role: "developer",projectname:"ACRT/BM"},
+    {username: "Surenthran", rollnumber: "VT173", role: "developer",projectname:"ECSITE"},
+    {username: "Muthunarayani", rollnumber: "VT178", role: "developer",projectname:"ECSITE"},
+    {username: "Priyanka", rollnumber: "VT211", role: "developer",projectname:"ECSITE"},
+    {username: "Govardhananan", rollnumber: "VT220", role: "developer",projectname:"WHITECUPS"},
+    {username: "Soumiksen", rollnumber: "VT221", role: "developer",projectname:"WHITECUPS"},
+    {username: "Harishkumar", rollnumber: "VT222", role: "developer",projectname:"WHITECUPS"},
+    {username: "Jegan Arul", rollnumber: "VT102", role: "developer",projectname:"ACRT/BM"},
+    {username: "Dinesh Nehru", rollnumber: "VT151", role: "developer",projectname:"ACRT/BM"},
+    {username: "Balaganeshwaran", rollnumber: "VT155", role: "developer",projectname:"ACRT/BM"},
+    {username: "Vignesh Selvabharathi", rollnumber: "VT170", role: "developer",projectname:"ACRT/BM"},
+    {username: "Ajithkumar paramashivam", rollnumber: "VT062", role: "developer",projectname:"DEOLDATA"},
+    {username: "Sanjeevkumar", rollnumber: "VT118", role: "developer",projectname:"DEOLDATA"},
+    {username: "Prabhu Selvaraj", rollnumber: "VT125", role: "developer",projectname:"DEOLDATA"},
+    {username: "Sebastian nisholin", rollnumber: "VT130", role: "developer",projectname:"DEOLDATA"},
+    {username: "Logeshwari sarangan", rollnumber: "VT132", role: "developer",projectname:"VRAP"},
+    {username: "Pichaimuthu kanagaraj", rollnumber: "VT134", role: "developer",projectname:"DEOLDATA"},
+    {username: "Dineshkumar Aanathachari", rollnumber: "VT152", role: "developer",projectname:"DEOLDATA"},
+    {username: "Naveen Raja", rollnumber: "VT200", role: "developer",projectname:"DEOLDATA"},
+    {username: "Vindhiya", rollnumber: "VT217", role: "developer",projectname:"DEOLDATA"}
+].each do | Project |
+    unless User.where(projectname: user[:projectname]).present?
+        User.create!(project)
+    end
+
+end
+
+[
+    {name: "Tea"},{name: "Milk"}
+].each do |Hotdrinks|
+    unless User.where(Hotdrinks:user[:Hotdrinks]).present?
+        user.create!(Hotdrinks)
+[
+    {snacks: "goodday"},
+    {snacks: "milkbikis"},
+    {snacks: "mariegold"},
+    {snacks: "burfi"},
+    {snacks: "treat"}
+    ].each do | Snacks options |
+        unless User.where(Snacks option:user[:Snacks Options]).present?
+            user.create!(Snacks Options)
+
+
